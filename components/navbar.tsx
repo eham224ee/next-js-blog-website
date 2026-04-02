d26041a1e14a60d0aacf10591db6cd09ec56cd6b
eham <eham.w.r@gmail.com>
2026-04-03 02:31:48 +0600

    finished navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,29 +1,42 @@
+'use client '
+
 import { navLinks } from '@/constants'
 import Link from 'next/link'
 import React from 'react'
+import { Button, buttonVariants } from './ui/button'
+import { ThemeToggle } from './themeToggle'
 
 const Navbar = () => {
   return (
-    <nav className='w-full h-24'>
+    <nav className='w-full h-24 flex items-center justify-between px-4'>
         <div>
-            <h1 className='text-3xl font-bold'>Next<span className='text-blue-500'>Pro</span></h1>
+            <Link href='/'>
+            <p className='text-4xl font-bold'>Next<span className='text-blue-500'>Pro</span></p>
+            </Link>
         </div>
         <div>
             {navLinks.map((link) => {
                 return (
-                    <Link key={link.route} href={link.route}>
-                        {link.label}
+                    <Link className={buttonVariants({ variant: 'ghost',
+                        className: 'mx-2 p-4 text-xl'
+                    })}  key={link.route} href={link.route}>
+                            {link.label}
                     </Link>
                 )
             })}
         </div>
-        <div>
-            <Link href='/sign-up'>
+        <div className='flex items-center gap-2'>
+           <Button className='hover:bg-olive-400'>
+             <Link href='/sign-up'  className='text-[18px]'>
                 Sign Up
             </Link>
-            <Link href='/login'>
+           </Button>
+            <Link className={buttonVariants({ variant: 'ghost', 
+               className: 'p-4 text-[18px]'
+            })} href='/login'>  
                 Login
             </Link>
+            <ThemeToggle />
         </div>
     </nav>
   )
